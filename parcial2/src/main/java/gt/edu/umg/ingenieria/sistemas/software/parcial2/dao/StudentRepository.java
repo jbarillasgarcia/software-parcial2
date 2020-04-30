0d99f11e10908f3b757d783c4e850103c1f812bb
dovalle90 <48726598+dovalle90@users.noreply.github.com>
2020-04-30 01:33:14 -0600

    Repository,Service & Controller

--- parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/dao/StudentRepository.java
+++ parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/dao/StudentRepository.java
@@ -1,12 +1,14 @@
 
 package gt.edu.umg.ingenieria.sistemas.software.parcial2.dao;
 
+import gt.edu.umg.ingenieria.sistemas.software.parcial2.entity.T2Student;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository {
+public interface StudentRepository extends CrudRepository <T2Student, Integer>{
     
-    
+    public List<T2Student> findAllByName(String name);
     
 }
