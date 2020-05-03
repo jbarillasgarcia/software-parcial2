c9418310b241e3e9c741e20e4051fdd7aa2fb6f0
rocordonez9 <rodrigocristobal9@gmail.com>
2020-05-02 21:52:29 -0600

    Creacion de rama

--- parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/Parcial2Application.java
+++ parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/Parcial2Application.java
@@ -8,9 +8,7 @@ public class Parcial2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Parcial2Application.class, args);
-                
-                
-                
+                                              
 	}
 
 }
