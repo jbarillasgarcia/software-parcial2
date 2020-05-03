6cca6786f1ee2df00cb7875061cb513c06cf0f0a
dlaguardia13 <51100069+dlaguardia13@users.noreply.github.com>
2020-05-02 23:44:59 -0600

    Merge dgarcialaguardia into parcial2
    Conflicts:
    	piva

--- parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/controller/T2StudentController.java
+++ parcial2/src/main/java/gt/edu/umg/ingenieria/sistemas/software/parcial2/controller/T2StudentController.java
@@ -1,37 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package gt.edu.umg.ingenieria.sistemas.software.parcial2.controller;
 
-import gt.edu.umg.ingenieria.sistemas.software.parcial2.entity.T2Student;
-import gt.edu.umg.ingenieria.sistemas.software.parcial2.services.T2StudentService;
-import java.util.List;
+import gt.edu.umg.ingenieria.sistemas.software.parcial2.service.T2StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author Josué Pivaral
+ * @author rodrigo
  */
+
 @RestController
 public class T2StudentController {
     
-    @Autowired
-    public T2StudentService studentService;
-    
-    @GetMapping("student/lazyLoad")
-    public List<T2Student> handle1(@RequestParam(name = "page") int page,
-                                   @RequestParam(name = "size") int size,
-                                   @RequestParam(name = "field") String field,
-                                   @RequestParam(name = "direction") String direction){     
-       return this.studentService.getStudentPerPages(page,size,field,direction);
-   
-   } 
-/**
- *
- * @author rodrigo
- */
     @Autowired
     private T2StudentService studentServ;
     
@@ -42,6 +30,5 @@ public class T2StudentController {
                        @RequestParam(name = "order") String orden,
                        @RequestParam(name = "limit") Integer limitVal){
         return this.studentServ.deleteLimitStoredProcedure(tableName, fildCond, fildVal, orden, limitVal);
-    
- }
+    }
 }
